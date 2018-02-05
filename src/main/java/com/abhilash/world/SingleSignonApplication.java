684c5560352196d4f3f88b26df49d278f209cce8
Abhilash Ghosh <abilash.ghosh@lidl.com>
2018-02-05 17:48:21 +0100

    A POC demonstrating spring-security with keyclock

---
+++ src/main/java/com/abhilash/world/SingleSignonApplication.java
@@ -0,0 +1,12 @@
+package com.abhilash.world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleSignonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleSignonApplication.class, args);
+	}
+}
